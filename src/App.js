9cf68418aeb37d78d37b039aea3f8df532b0a80f
Rashi Trivedi <trivedi.rashi95@gmail.com>
2023-10-31 08:31:41 +0530

    upload folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("dummyUser");
+  const [chats,setChats] = useState([
+    {name:'user1',message:'message1'},
+    {name:'dummyUser',message:'message2'}
+  ]);
+
+  const [msg,setMsg] = useState('');
+
+  const sendChat= ()=>{
+    const c =[...chats];
+    c.push({name,message:msg});
+    setChats(c);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div > 
+      <h1>User:{name}</h1>
+      <div className='chat-container'>
+         {chats.map(c=><div className={'container ${c.name===name? }'}>
+            <p className='chatbox'>
+            <strong>{c.name}</strong>
+            <span>{c.message}</span>
+            </p>
+         </div>)}
+        </div>
+         
+         <div className='btm'>
+           <input type="text" onInput={e=>setMsg(e.target.value) }
+           placeholder='enter your chat'/>
+           <button onClick={e=>sendChat()}>Send</button>
+         </div>
     </div>
   );
 }
